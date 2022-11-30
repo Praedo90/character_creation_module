c411addaf9027c52baf9a68867d4dce98568c75c
unknown <zverys95@gmail.com>
2022-11-30 22:01:41 +0300

    Change main.py

--- main.py
+++ main.py
@@ -2,6 +2,72 @@ from random import randint
 
 from graphic_arts.start_game_banner import run_screensaver
 
+"""значение стандартной атаки."""
+DEFAULT_ATTACK = 5
+"""стандартное значение защиты."""
+DEFAULT_DEFENCE = 10
+DEFAULT_STAMINA = 80
+
+
+class Character:
+
+    BRIEF_DESC_CHAR_CLASS = 'отважный любитель приключений'
+    RANGE_VALUE_ATTACK = (1, 3)  # диапазона очков урона.
+    RANGE_VALUE_DEFENCE = (1, 5)  # диапазон значения защиты.
+    SPECIAL_BUFF = 15  # значение очков урона, для базового класса.
+    SPECIAL_SKILL = 'Удача'
+
+    # Объявляем конструктор класса.
+    def __init__(self, name):
+        self.name = name
+
+    # Объявляем метод атаки.
+    def attack(self):
+        # Описываем метод атаки.
+        value_attack = DEFAULT_ATTACK + randint(*self.RANGE_VALUE_ATTACK)
+        return (f'{self.name} нанёс противнику урон, равный {value_attack}')
+
+    # Объявляем метод защиты.
+    def defence(self):
+        value_defence = DEFAULT_DEFENCE + randint(*self.RANGE_VALUE_DEFENCE)
+        return (f'{self.name} блокировал {value_defence} ед. урона.')
+
+    # Объявляем метод специального умения.
+    def special(self):
+        return (f'{self.name} применил специальное умение '
+                f'"{self.SPECIAL_SKILL} {self.SPECIAL_BUFF}".')
+
+    # Новый метод базового класса.
+    def __str__(self):
+        return f'{self.__class__.__name__} - {self.BRIEF_DESC_CHAR_CLASS}.'
+
+
+class Warrior(Character):
+    BRIEF_DESC_CHAR_CLASS = (' дерзкий воин ближнего боя. '
+                             'Сильный, выносливый и отважный')
+    RANGE_VALUE_ATTACK = (3, 5)
+    RANGE_VALUE_DEFENCE = (5, 10)
+    SPECIAL_BUFF = DEFAULT_STAMINA + 25
+    SPECIAL_SKILL = 'Выносливость'
+
+
+class Mage(Character):
+    BRIEF_DESC_CHAR_CLASS = (' находчивый воин дальнего боя. '
+                             'Обладает высоким интеллектом')
+    RANGE_VALUE_ATTACK = (5, 10)
+    RANGE_VALUE_DEFENCE = (-2, 2)
+    SPECIAL_BUFF = DEFAULT_ATTACK + 40
+    SPECIAL_SKILL = 'Атака'
+
+
+class Healer(Character):
+    BRIEF_DESC_CHAR_CLASS = (' могущественный заклинатель. '
+                             'Черпает силы из природы, веры и духов')
+    RANGE_VALUE_ATTACK = (-3, -1)
+    RANGE_VALUE_DEFENCE = (2, 5)
+    SPECIAL_BUFF = DEFAULT_DEFENCE + 30
+    SPECIAL_SKILL = 'Защита'
+
 
 def attack(char_name: str, char_class: str) -> str:
     """Нанесенный урон."""
@@ -17,7 +83,11 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
-    """Защита."""
+    """
+    Принимает на вход имя и класс персонажа.
+    Возвращает строку сообщения о блокированном персонажем уроне
+    в зависимости от его класса.
+    """
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
